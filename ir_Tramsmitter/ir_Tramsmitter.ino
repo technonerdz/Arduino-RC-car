7d41b713e7051fc90e0546539db6fac539611be1
Félix Giffard <technonerdzfg@gmail.com>
2017-03-07 12:00:16 -0500

    test Transmitter and receiver files
    These are only to test if everything is working

---
+++ ir_Tramsmitter/ir_Tramsmitter.ino
@@ -0,0 +1,34 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+
+IRsend irsend;
+
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+irsend.sendSony(0xa90, 12); // Sony TV power code
+delay(50);
+
+irsend.sendSony(0xa91, 12); // Sony TV power code
+delay(50);
+
+irsend.sendSony(0xa92, 12); // Sony TV power code
+delay(50);
+
+irsend.sendSony(0xa93, 12); // Sony TV power code
+delay(50);
+  
+//   for (int i = 0; i < 50; i++) { 
+//     irsend.sendSony(0xa90, 12); // Sony TV power code
+//     delay(40);
+//   }
+//   for (int i = 0; i < 50; i++) { 
+//     irsend.sendSony(0xa92, 12); // Sony TV power code
+//     delay(40);
+//   }
+}
